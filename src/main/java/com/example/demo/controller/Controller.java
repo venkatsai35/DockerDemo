e778ac5ef6f244ce207ef674158c3201cc8fd16a
venkatsai35 <118942577+venkatsai35@users.noreply.github.com>
2022-12-30 16:11:02 -0600

    Add files via upload

---
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class Controller {
+	
+
+	@GetMapping("/employee")
+	public String restTemplate()
+	{
+		
+		return "Getting Employees";
+		
+	}
+
+
+}
